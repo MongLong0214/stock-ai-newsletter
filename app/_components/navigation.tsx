8f33f95e28da12539cae1d2991e838e0fca31835
MongLong <weplay0628@gmail.com>
2025-10-30 10:13:31 +0900

    버튼 삭제

--- app/_components/navigation.tsx
+++ app/_components/navigation.tsx
@@ -3,7 +3,6 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
-import { Button } from '@/components/ui/button';
 
 function Navigation() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
